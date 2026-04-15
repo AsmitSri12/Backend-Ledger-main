9309562c9843e4eed75c9df6668d788f17da4517
AsmitSri12 <asmitsri2182@gmail.com>
2026-04-16 01:53:50 +0530

    Introducing Resend api instead of using Nodemailer.

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,46 +1,9 @@
-const nodemailer = require("nodemailer");
-require("dotenv").config();
+const { Resend } = require("resend");
 const fs = require("fs");
 const path = require("path");
 const handlebars = require("handlebars");
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    type: "OAuth2",
-    user: process.env.EMAIL_USER,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken: process.env.REFRESH_TOKEN,
-  },
-});
-
-// Verify the connection configuration
-transporter.verify((error, success) => {
-  if (error) {
-    console.error("Warning: Email server connection failed (likely missing credentials). Emails will not be sent.");
-  } else {
-    console.log("Email server is ready to send messages");
-  }
-});
-
-// Function to send email
-const sendEmail = async (to, subject, text, html) => {
-  try {
-    const info = await transporter.sendMail({
-      from: `"Backend Ledger" <${process.env.EMAIL_USER}>`, // sender address
-      to, // list of receivers
-      subject, // Subject line
-      text, // plain text body
-      html, // html body
-    });
-
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  } catch (error) {
-    console.error("Error sending email:", error);
-  }
-};
+const resend = new Resend("re_NSa81nqz_FrhcX3otHLGUxy9LsQnUek2n");
 
 const compileTemplate = (templateName, data) => {
   const filePath = path.join(__dirname, "../templates", `${templateName}.hbs`);
@@ -48,6 +11,21 @@ const compileTemplate = (templateName, data) => {
   const template = handlebars.compile(source);
   return template(data);
 };
+
+async function sendEmail(to, subject, text, html) {
+  try {
+    const data = await resend.emails.send({
+      from: "Backend Ledger <onboarding@resend.dev>",
+      to,
+      subject,
+      text,
+      html,
+    });
+    console.log("Email sent:", data);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+}
 
 async function sendRegistrationEmail(userEmail, name) {
   const subject = "Welcome to Backend Ledger!";
@@ -77,6 +55,4 @@ module.exports = {
   sendRegistrationEmail,
   sendTransactionEmail,
   sendTransactionFailureEmail,
-}
-
-
+};
